70f1a3b42102f352db61d02b0f5f882f80b5bdf6
TaranisElsu <taraniselsu@gmail.com>
2022-05-24 22:23:49 -0400

    Imported my screenshot script

---
+++ GameDevTV2022/Assets/_Project/Editor/Screenshot.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace Common.Editor
+{
+    public class Screenshot
+    {
+        [MenuItem("Tools/Take Screenshot")]
+        internal static void TakeScreenshot()
+        {
+            string date = DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss");
+            string filename = $"screenshot_{date}.png";
+            Debug.Log($"Screenshot {filename}");
+            ScreenCapture.CaptureScreenshot(filename);
+        }
+    }
+}
