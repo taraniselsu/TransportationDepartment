51e8e5403bc8bf8372399e79fbaa628380df56e0
TaranisElsu <taraniselsu@gmail.com>
2022-05-28 22:55:57 -0400

    Predefine the levels instead of randomly spawning departments

---
+++ GameDevTV2022/Assets/_Project/Scripts/DepartmentDefinition.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public struct DepartmentDefinition
+{
+    public enum Direction { North, East, South, West };
+
+    public string name;
+    public Vector2Int position;
+    public Direction direction;
+}
--- GameDevTV2022/Assets/_Project/Scripts/DepartmentSpawner.cs
+++ GameDevTV2022/Assets/_Project/Scripts/DepartmentSpawner.cs
@@ -1,56 +1,24 @@
-using System.Linq;
 using TMPro;
 using UnityEngine;
-using Random = UnityEngine.Random;
 
 public class DepartmentSpawner : MonoBehaviour
 {
-    [SerializeField] private TextAsset departmentNamesFile;
+    [SerializeField] private LevelDefinition levelDefinition;
     [SerializeField] private GameObject departmentPrefab;
-    [SerializeField] private int numDepartments = 3;
     [SerializeField] private float gridSize = 18f;
-    [SerializeField] private Bounds bounds = new(Vector3.zero, new Vector3(10, 0, 10));
 
     private void Start()
     {
-        string text = departmentNamesFile.text;
-        Debug.Log(text);
-
-        string[] departmentNames = text.Split("\r\n", System.StringSplitOptions.RemoveEmptyEntries);
-        Debug.LogFormat(this, "Department names: {0}", string.Join(", ", departmentNames));
-
-        var names = departmentNames.OrderBy(n => Random.value).Take(numDepartments);
-        foreach (string name in names)
+        foreach (DepartmentDefinition department in levelDefinition.departments)
         {
-            GameObject department = Instantiate(departmentPrefab);
-            department.name = name;
-            department.GetComponentInChildren<TextMeshProUGUI>().text = name;
+            Vector3 position = new(department.position.x * gridSize, 0, department.position.y * gridSize);
+            Quaternion rotation = Quaternion.Euler(0, 90f * (int)department.direction, 0);
 
-            Vector3 position = GetRandomPosition();
-            Quaternion rotation = Quaternion.Euler(0, 90f * Random.Range(0, 4), 0);
+            GameObject go = Instantiate(departmentPrefab, position, rotation);
+            go.name = department.name;
+            go.GetComponentInChildren<TextMeshProUGUI>().text = department.name;
 
-            department.transform.SetPositionAndRotation(position, rotation);
-
-            Debug.LogFormat(this, "Spawning {0} at {1}", name, position);
+            Debug.LogFormat(this, "Spawning {0} at {1} facing {2}", department.name, position, department.direction);
         }
-    }
-
-    private Vector3 GetRandomPosition()
-    {
-        float x1 = Random.Range(bounds.min.x, bounds.max.x);
-        float x2 = Random.Range(bounds.min.x, bounds.max.x);
-        float x = (x1 + x2) / 2f;
-
-        float z1 = Random.Range(bounds.min.z, bounds.max.z);
-        float z2 = Random.Range(bounds.min.z, bounds.max.z);
-        float z = (z1 + z2) / 2f;
-
-        return new Vector3(Mathf.RoundToInt(x) * gridSize, 0, Mathf.RoundToInt(z) * gridSize);
-    }
-
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.green;
-        Gizmos.DrawWireCube(bounds.center * gridSize, bounds.size * gridSize);
     }
 }
